a774217c0473ee73fc01e6c1ef774f8f1255607d
Xavier-Emmanuel Moreau <xmoreau@student.le-101.fr>
2019-11-25 14:06:29 +0100

    suite video

--- src/ft_nm.c
+++ src/ft_nm.c
@@ -11,20 +11,59 @@
 /*                                                        /                   */
 /* ************************************************************************** */
 
+#include <mach-o/nlist.h>
 #include "../includes/ft_nm.h"
+
+int 		read_symtab(struct symtab_command *lc, t_manager *manager)
+{
+	int 	i;
+	struct nlist_64 *el;
+	char			*stringtable;
+
+	el = (struct nlist_64 *)(manager->file + lc->symoff);
+	stringtable = manager->file + lc->stroff;
+	i = 0;
+	while (i < lc->nsyms)
+	{
+		ft_printf("%s\n", stringtable + el[i].n_un.n_strx);
+		i++;
+	}
+	return (TRUE);
+}
+
+int			handle_64(t_manager *manager)
+{
+	struct mach_header_64	*header;
+	struct load_command		*lc;
+	int 					i;
+	struct symtab_command	*sym;
+
+	i = 0;
+	header = (struct mach_header_64*)manager->file;
+	lc = (struct load_command*)((void*)manager->file + sizeof(struct mach_header_64));
+	while (i < header->ncmds)
+	{
+		if (lc->cmd == LC_SYMTAB)
+		{
+			sym = (struct symtab_command*)lc;
+			break ;
+		}
+		lc = (void*)lc + lc->cmdsize;
+		i++;
+	}
+	return (TRUE);
+}
 
 int 		nm(t_manager *manager)
 {
 	uint32_t	magic;
-	struct mach_header_64 *header;
 
 	magic = *(uint32_t*)manager->file;
+	printf("magic = %#x\n", *(int*)ft_memrev(&magic, 1, 4));
 	if (magic == MH_MAGIC_64)
 	{
-		header = (struct mach_header_64*)manager->file;
-		printf("file type = %#x\n", header->filetype);
+		handle_64(manager);
 	}
-	printf("magic = %#x\n", *(int*)ft_memrev(&magic, 1, 4));
 	return (TRUE);
 }
 
