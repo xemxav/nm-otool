5b981406f4f2b25d1b45973d4488be8bfc587a4c
Xavier-Emmanuel Moreau <xmoreau@student.le-101.fr>
2019-12-03 12:35:24 +0100

    lecteur de lc segment ok

--- src/ft_nm.c
+++ src/ft_nm.c
@@ -14,72 +14,110 @@
 #include <mach-o/nlist.h>
 #include "../includes/ft_nm.h"
 
-int 		record_section(uint8_t n_sect, t_manager *manager,
-		struct segment_command_64 *segment, uint32_t sum_nsects)
+//int 		record_section(uint8_t n_sect, t_manager *manager,
+//		struct segment_command_64 *segment, uint32_t sum_nsects)
+//{
+//	struct section_64	*section;
+//
+//	section = (struct section_64*)((void*)manager + segment->fileoff);
+//	while (sum_nsects != n_sect)
+//	{
+//		sum_nsects++;
+//		section = (void*)section + section->size;
+//	}
+//
+//}
+
+//int			get_section(uint8_t n_sect, t_manager *manager)
+//{
+//	struct load_command			*lc;
+//	int 						i;
+//	struct segment_command_64	*segment;
+//	uint32_t					sum_nsects;
+//
+//	i = 0;
+//	sum_nsects = 0;
+//	lc = (struct load_command*)((void*)manager->file + manager->header_size);
+//	while (i < manager->ncmds)
+//	{
+//		if (lc->cmd == LC_SEGMENT)
+//		{
+//			segment = (struct segment_command_64*)lc;
+//			if ((sum_nsects + segment->nsects) >= n_sect)
+//				return (record_section(n_sect, manager, segment, sum_nsects));
+//			sum_nsects += segment->nsects;
+//		}
+//		lc = (void*)lc + lc->cmdsize;
+//		i++;
+//	}
+//	return (FALSE);
+//}
+//
+//int 		get_symbol(t_symbol *symbol, uint8_t n_type, uint8_t n_sect, t_manager *manager)
+//{
+//	if ((n_type & N_TYPE) == N_UNDF)
+//		*symbol->sym_name = 'U';
+//	if ((n_type & N_TYPE) == N_SECT)
+//		get_section(n_sect, manager);
+//	return (TRUE);
+//}
+//
+//int 		read_symtab(struct symtab_command *sym, t_manager *manager)
+//{
+//	int 	i;
+//	struct nlist_64 *el;
+//	char			*stringtable;
+//	t_symbol		symbol;
+//
+//	el = (struct nlist_64 *)(manager->file + sym->symoff);
+//	stringtable = manager->file + sym->stroff;
+//	i = 0;
+//
+//	while (i < sym->nsyms)
+//	{
+//		ft_bzero(&symbol, sizeof(t_symbol));
+//		symbol.sym_name = stringtable + el[i].n_un.n_strx;
+//		get_symbol(&symbol, el[i].n_type , el[i].n_sect, manager);
+//		ft_printf("type = %#x, %s\n", el[i].n_type & N_TYPE, stringtable + el[i].n_un.n_strx);
+//		i++;
+//	}
+//	return (TRUE);
+//}
+//
+//int			handle_64(t_manager *manager)
+//{
+//	struct mach_header_64	*header;
+//	struct load_command		*lc;
+//	int 					i;
+//
+//	i = 0;
+//	header = (struct mach_header_64*)manager->file;
+//	lc = (struct load_command*)((void*)manager->file + sizeof(struct mach_header_64));
+//	manager->ncmds = header->ncmds;
+//	while (i < header->ncmds)
+//	{
+//		if (lc->cmd == LC_SYMTAB)
+//		{
+//			read_symtab((struct symtab_command*)lc, manager);
+//			break ;
+//		}
+//		lc = (void*)lc + lc->cmdsize;
+//		i++;
+//	}
+//	return (TRUE);
+//}
+
+int			read_lc_segment(struct segment_command_64 *segment, char *file)
 {
-	struct section_64	*section;
-
-	section = (struct section_64*)((void*)manager + segment->fileoff);
-	while (sum_nsects != n_sect)
+	int i = 0;
+	struct section_64 *section;
+	ft_printf("segname = %s et nsects = %d\n", segment->segname, segment->nsects);
+	section = (struct section_64*)((void*)segment + sizeof(struct segment_command_64));
+	while (i < segment->nsects)
 	{
-		sum_nsects++;
-		section = (void*)section + section->size;
-	}
-
-}
-
-int			get_section(uint8_t n_sect, t_manager *manager)
-{
-	struct load_command			*lc;
-	int 						i;
-	struct segment_command_64	*segment;
-	uint32_t					sum_nsects;
-
-	i = 0;
-	sum_nsects = 0;
-	lc = (struct load_command*)((void*)manager->file + manager->header_size);
-	while (i < manager->ncmds)
-	{
-		if (lc->cmd == LC_SEGMENT)
-		{
-			segment = (struct segment_command_64*)lc;
-			if ((sum_nsects + segment->nsects) >= n_sect)
-				return (record_section(n_sect, manager, segment, sum_nsects));
-			sum_nsects += segment->nsects;
-		}
-		lc = (void*)lc + lc->cmdsize;
+		ft_printf("sec num: %d sectname = %s, segname = %s\n", i + 1, section[i].sectname, section[i].segname);
 		i++;
-	}
-	return (FALSE);
-}
-
-int 		get_symbol(t_symbol *symbol, uint8_t n_type, uint8_t n_sect, t_manager *manager)
-{
-	if ((n_type & N_TYPE) == N_UNDF)
-		*symbol->sym_name = 'U';
-	if ((n_type & N_TYPE) == N_SECT)
-		get_section(n_sect, manager);
-	return (TRUE);
-}
-
-int 		read_symtab(struct symtab_command *sym, t_manager *manager)
-{
-	int 	i;
-	struct nlist_64 *el;
-	char			*stringtable;
-	t_symbol		symbol;
-
-	el = (struct nlist_64 *)(manager->file + sym->symoff);
-	stringtable = manager->file + sym->stroff;
-	i = 0;
-
-	while (i < sym->nsyms)
-	{
-		ft_bzero(&symbol, sizeof(t_symbol));
-		symbol.sym_name = stringtable + el[i].n_un.n_strx;
-		get_symbol(&symbol, el[i].n_type , el[i].n_sect, manager);
-		ft_printf("type = %#x, %s\n", el[i].n_type & N_TYPE, stringtable + el[i].n_un.n_strx);
-		i++;
+//		section = (struct section_64*)((void*)section + section->size);
 	}
 	return (TRUE);
 }
@@ -89,6 +127,7 @@ int			handle_64(t_manager *manager)
 	struct mach_header_64	*header;
 	struct load_command		*lc;
 	int 					i;
+//	;
 
 	i = 0;
 	header = (struct mach_header_64*)manager->file;
@@ -96,17 +135,18 @@ int			handle_64(t_manager *manager)
 	manager->ncmds = header->ncmds;
 	while (i < header->ncmds)
 	{
-//		enregistré dans manager le offset pour le header et ncmds pour ne pas devoir jouer avec les types
-		if (lc->cmd == LC_SYMTAB)
-		{
-			read_symtab((struct symtab_command*)lc, manager);
-			break ;
-		}
+		ft_printf("lc->cmd : %#x\n", lc->cmd);
+		if (lc->cmd == LC_SEGMENT_64)
+			read_lc_segment((struct segment_command_64*)lc, manager->file);
+//		if (lc->cmd == LC_SYMTAB);
+//			read_symtan();
 		lc = (void*)lc + lc->cmdsize;
 		i++;
 	}
 	return (TRUE);
 }
+
+
 
 int 		nm(t_manager *manager)
 {
