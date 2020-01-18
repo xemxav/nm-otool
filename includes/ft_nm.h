47e34c5d897070a8330dc9a71ce8977ef48066d1
Xavier-Emmanuel Moreau <xmoreau@student.le-101.fr>
2020-01-18 16:28:59 +0100

    swap functions

--- includes/ft_nm.h
+++ includes/ft_nm.h
@@ -39,7 +39,7 @@ typedef struct				s_libfile
 {
 	char					*filename;
 	char 					*file_start;
-	uint32_t 				offset;
+	uint64_t 				offset;
 	struct s_libfile		*next;
 }							t_libfile;
 
@@ -51,7 +51,7 @@ typedef struct				s_manager
 	int 					fd;
 	struct stat				buf; //verifier si besoin de le garder dans le manager
 	int 					swap;
-	struct symtab_command	*symtab;
+	struct symtab_command	symtab;
 	size_t					header_size;
 	uint32_t				ncmds;
 	struct s_symbol 		*symbol_list;
@@ -62,7 +62,7 @@ typedef struct				s_manager
 
 
 int 				nm(t_manager *manager);
-void				study_type(t_symbol *symbol, uint8_t n_type, int swapping);
+void				study_type(t_symbol *symbol, uint8_t type);
 /*
  *			commons.c
  */
@@ -93,7 +93,7 @@ int					record_libfile(t_manager *manager, t_libfile *libfile);
 int   				iterate_on_libfile(t_manager *manager);
 
 /*
- *			symnols_64.c
+ *			symbols_64.c
  */
 int					record_symbol(t_manager *manager, t_symbol *symbol);
 void				print_symbols_64(t_manager *manager);
@@ -101,7 +101,16 @@ void				print_symbols_32(t_manager *manager);
 /*
  *			utils.c
  */
-void		*ts(void *tmp, void *value, size_t size, int ts);
+struct load_command *swap_lc(struct load_command *temp, struct load_command *lc);
+struct symtab_command *swap_symtab(struct symtab_command *temp,
+								   struct symtab_command *symtab);
+struct nlist *swap_nlist(struct nlist *temp, struct nlist *nlist);
+struct nlist_64 *swap_nlist64(struct nlist_64 *temp, struct nlist_64 *nlist);
+uint32_t add_nsect(uint32_t *nsects, int swap);
+
+/*
+ *			utils.c
+ */
 void 				*swap(void *tmp, void *value, size_t size);
 int					free_manager(t_manager *manager, int ret);
 t_symbol 			*free_symbols(t_symbol *head);
--- testing.sh
+++ testing.sh
@@ -6,14 +6,14 @@ NM_PERSO="./ft_nm"
 test_nm()
 {
   echo $1
-  (eval ${NM_PERSO} $1) > my_result
-  (eval ${NM} $1) > off_result
-  diff my_result off_result
+  eval ${NM_PERSO} $1 > my_result.txt
+  eval ${NM} $1 > off_result.txt
+  diff my_result.txt off_result.txt
     if [ $? -eq 0 ]
     then
       echo ' ok'
-      rm my_result
-      rm off_result
+      rm my_result.txt
+      rm off_result.txt
     else
       echo $?
     fi
